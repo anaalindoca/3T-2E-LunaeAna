31f8679cec6467a92086a8595ffd3a4de6cfdcd5
nana <160139950+anaalindoca@users.noreply.github.com>
2024-10-28 08:53:43 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 function criaCartao(categoria,pergunta,resposta){
-console.log(categoria,pergunta,resposta)
+let container = document.getElementById ("container")
+let article = document.createElement("article")
+article.className = "cartao"
+article.innerHTML = "ANAELUNA"
+container.appendChild (article)
 }
+
 
 criaCartao("Biologia","Qual a função da mitocondria","criar atp, para geração de energia")
